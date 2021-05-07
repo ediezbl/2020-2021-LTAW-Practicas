60b9ea843db0ac3cab48685cba6c7a62aa7059d7
ediez <eduardodiezblanco@gmail.com>
2021-05-07 16:26:36 +0200

    Ejemplo hola mundo node.js

---
+++ P1/P1-S1/ej-01-hello.js
@@ -0,0 +1,9 @@
+// -- Programa de ejemplo Hola mundo en node.js
+
+/* Este es un ejemplo de comentario 
+multilínea
+El objeto console está disponible directamente desde node.js
+no es necesario importar nada*/
+
+// -- Imprimir un mensaje en la consola
+console.log("¡Hola Mundo!");
