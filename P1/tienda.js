b2954a977655872425feb4b64b7c024adb015cd9
ediez <eduardodiezblanco@gmail.com>
2021-05-10 23:49:05 +0200

    Intentando servir el estilo

--- P1/tienda.js
+++ P1/tienda.js
@@ -2,28 +2,34 @@
 // Importando los modulos 
 const http = require('http');
 const fs = require('fs');
-const puerto = 9000;
-let main_page;
-// Leer el html utilizando el módulo fs
-const file = 'tienda.html';
-fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-        console.log("Error");
-        console.log(err.message);
-    } else {
-        console.log("Lectura Completada...");
-        main_page = data;
-    }
-});
-
+let path = require('path');
+const port = 9000;
 // Iniciando el servidor 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.statusMessage = "OK";
-    res.setHeader('Content-Type', 'text/html');
-    res.write(main_page);
-    res.end();
+    // Extrayendo la ruta del archivo pedido
+    let fileRoute = '.' + ((req.url == '/')?'tienda.html':req.url);
+    // Ahora extraer la extension 
+    let ext = path.extname(fileRoute);
+    let contentType = 'text/html' // Esto es el tipo de archivo que va a servir por defecto
+    // Ahora miramos los diferentes tipos de archivos disponibles 
+    switch(ext) {
+        case '.css':
+            contentType = 'text/css'
+    }
+    // Voy a leer los archivos del sistema de ficheros 
+    fs.readFile(fileRoute, 'utf8', (err, data) => {
+        if (err) { // Si ha ocurrido algun error como por ejemplo el fichero no existe
+            console.log("Error!!");
+            console.log(err.message);
+        } else { // no hay error, se produce la lectura normal
+            console.log("Lectura completada...");
+            res.writeHead(200, 'Content-Type', contentType);
+            res.write(data);
+            res.end();
+        }
+    });
+
 });
 
-server.listen(puerto);
+server.listen(port);
 console.log("Servidor tienda activado. Escuchando en el puerto: " + puerto);
