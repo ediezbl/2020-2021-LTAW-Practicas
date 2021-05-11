ebc1b5c5000eaa53d9181ad8d7e8d31bf242c3e1
ediez <eduardodiezblanco@gmail.com>
2021-05-11 11:42:51 +0200

    el servidor ya sirve html y css

--- P1/tienda.js
+++ P1/tienda.js
@@ -2,16 +2,39 @@
 // Importando los modulos 
 const http = require('http');
 const fs = require('fs');
-let path = require('path');
 const port = 9000;
 
 // Iniciando el servidor 
 const server = http.createServer((req, res) => {
+    // Formando la URL
+    const url = new URL(req.url, 'http://' + req.headers['host']);
+    path = url.pathname;
+    console.log(path);
+    let filename = "";
+    let contentType = "";
+    // Colocando el nombre del archivo y su contentType
+    if(path == '/') {
+        filename = 'tienda.html';
+        contentType = 'text/html';
+    } else if (path == '/tienda.css') {
+        filename = 'tienda.css';
+        contentType = 'text/css';
+    }
     // Extrayendo la ruta del archivo pedido
-    res.statusCode = 200;
-    res.statusMessage = "OK";
-    res.write("Happy server");
-    res.end();
+    if (path != '/favicon.ico') {
+        // Si el path es distinto de favicon.ico 
+        // entonces puedo empezar la lectura de archivos.
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) { // Si ha ocurrido algun error como por ejemplo el fichero no existe
+                console.log("Error!!");
+                console.log(err.message);
+            } else { // no hay error, se produce la lectura normal
+                res.writeHead(200, {'Content-Type': contentType});
+                res.write(data);
+                res.end();
+            }
+        });
+    }
 });
 
 server.listen(port);
