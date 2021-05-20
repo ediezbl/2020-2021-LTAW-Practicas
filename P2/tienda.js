9575a6c09b2284c9046a2ac0ee6c21c3570be395
ediez <eduardodiezblanco@gmail.com>
2021-05-20 22:19:30 +0200

    falta boton para busqueda

--- P2/tienda.js
+++ P2/tienda.js
@@ -62,8 +62,8 @@ function get_productos(req) {
 // Iniciando el servidor 
 const server = http.createServer((req, res) => {
     // Variables
-    let filename = "";
-    let contentType = "";
+    let filename = ""; // por defecto entrega la tienda
+    let contentType = ""; // por defecto entrega texto html
     let resCode = 200;
     let content = "";
     let html_extra = "";
@@ -84,9 +84,42 @@ const server = http.createServer((req, res) => {
     // Comprobar el path 
     if(path == '/') {
         filename = 'tienda.html';
-    } else {
+    } else if (path == '/productos') {
+        // Si el path es /productos voy a atender a la peticion AJAX
+        contentType = "application/json";
+        // obteniendo los parametros de la url
+        let param1 = url.searchParams.get('param1');
+        // pasarlo a minusculas porque mis productos estan en minuscula 
+        param1 = param1.toLowerCase();
+        // Rellenando un array con los productos de la tienda 
+        let products = [tienda[0]["productos"][0]["nombre"],
+                        tienda[0]["productos"][1]["nombre"], 
+                        tienda[0]["productos"][2]["nombre"]];
+        let result = [];
+        // Recorrer el array de productos 
+        for (let prod of products) {
+
+            //-- Pasar a mayúsculas
+            prodU = prod.toLowerCase();
+
+            //-- Si el producto comienza por lo indicado en el parametro
+            //-- meter este producto en el array de resultados
+            if (prodU.startsWith(param1)) {
+                result.push(prod);
+            }
+        }
+         // Escribir en formato JSON 
+         content = JSON.stringify(result);
+         // Enviar la respuesta 
+         res.writeHead(resCode, {'Content-Type': contentType});
+         res.write(content);
+         res.end();
+    }else {
         filename = path.split('/')[1];
         switch(filename) {
+            case 'tiendaAJAX.js':
+                contentType = 'application/javascript';
+                break;
             case 'barcelona':
                 contentType = 'text/html';
                 filename = 'barcelona.html'
@@ -199,65 +232,67 @@ const server = http.createServer((req, res) => {
                 break;
         }
     }
-    if (path != '/favicon.ico') {
-        // Si el path es distinto de favicon.ico 
-        // entonces puedo empezar la lectura de archivos.
-        fs.readFile(filename, (err, data) => {
-            if (err) { // Si ha ocurrido algun error como por ejemplo el fichero no existe
-                console.log("Error!!");
-                console.log(err.message);
-            } else { // no hay error, se produce la lectura normal
-                if (filename == 'tienda.html') {
-                    let user = get_user(req);
-                    if (user) {
-                        // Si ha encontrado el usuario, es decir, existia el campo con la cookie
-                        console.log(user);
-                        content = fs.readFileSync('tienda.html', 'utf-8');
-                        data = content.replace('Login', user);
-                        data = data.replace('/login', '');
-                        tienda[2]["username"] = user;
-                        // Escribiendo en el fihero JSON
-                        let myJSON = JSON.stringify(tienda);
-                        fs.writeFileSync(FICHERO_JSON, myJSON);
-                    } else {
-                        // Si no ha encontrado el campo con la cookie 
-                        if (user_name) {
-                            // Mirar si el usuario se acaba de registrar
-                            data = content.replace('Login', user_name);
-                            data = data.replace('/login', ''); 
+    if (path != '/favicon.ico'){
+        if (filename != "") {
+            // Si el path es distinto de favicon.ico 
+            // entonces puedo empezar la lectura de archivos.
+            fs.readFile(filename, (err, data) => {
+                if (err) { // Si ha ocurrido algun error como por ejemplo el fichero no existe
+                    console.log("Error!!");
+                    console.log(err.message);
+                } else { // no hay error, se produce la lectura normal
+                    if (filename == 'tienda.html') {
+                        let user = get_user(req);
+                        if (user) {
+                            // Si ha encontrado el usuario, es decir, existia el campo con la cookie
+                            console.log(user);
+                            content = fs.readFileSync('tienda.html', 'utf-8');
+                            data = content.replace('Login', user);
+                            data = data.replace('/login', '');
+                            tienda[2]["username"] = user;
+                            // Escribiendo en el fihero JSON
+                            let myJSON = JSON.stringify(tienda);
+                            fs.writeFileSync(FICHERO_JSON, myJSON);
                         } else {
-                            // Si el usuario no esta registrado, o no se acaba de registrar 
-                            // no se puede acceder al formulario de compra 
-                            let content = fs.readFileSync('tienda.html', 'utf-8');
-                            data = content.replace('formCompra.html', "");
-                        }
-                    } 
+                            // Si no ha encontrado el campo con la cookie 
+                            if (user_name) {
+                                // Mirar si el usuario se acaba de registrar
+                                data = content.replace('Login', user_name);
+                                data = data.replace('/login', ''); 
+                            } else {
+                                // Si el usuario no esta registrado, o no se acaba de registrar 
+                                // no se puede acceder al formulario de compra 
+                                let content = fs.readFileSync('tienda.html', 'utf-8');
+                                data = content.replace('formCompra.html', "");
+                            }
+                        } 
+                    }
+                    if (filename == 'barcelona.html') {
+                        html_extra = tienda[0]["productos"][0]["descripcion"];
+                        data = content.replace('HTML_EXTRA', html_extra);
+                    }
+                    if (filename == 'monza.html') {
+                        html_extra = tienda[0]["productos"][1]["descripcion"];
+                        data = content.replace('HTML_EXTRA', html_extra);
+                    }
+                    if (filename == 'monaco.html') {
+                        html_extra = tienda[0]["productos"][2]["descripcion"];
+                        data = content.replace('HTML_EXTRA', html_extra);
+                    }
+                    if (filename == 'formCompra.html') {
+                        let products = get_productos(req);
+                        if (products[0] != []) {
+                            products.forEach((element) => {
+                                html_extra += element + " ";
+                            });
+                        } 
+                        data = content.replace('HTML_EXTRA', html_extra);
+                    }
+                    res.writeHead(resCode, {'Content-Type': contentType});
+                    res.end(data);
                 }
-                if (filename == 'barcelona.html') {
-                    html_extra = tienda[0]["productos"][0]["descripcion"];
-                    data = content.replace('HTML_EXTRA', html_extra);
-                }
-                if (filename == 'monza.html') {
-                    html_extra = tienda[0]["productos"][1]["descripcion"];
-                    data = content.replace('HTML_EXTRA', html_extra);
-                }
-                if (filename == 'monaco.html') {
-                    html_extra = tienda[0]["productos"][2]["descripcion"];
-                    data = content.replace('HTML_EXTRA', html_extra);
-                }
-                if (filename == 'formCompra.html') {
-                    let products = get_productos(req);
-                    if (products[0] != []) {
-                        products.forEach((element) => {
-                            html_extra += element + " ";
-                        });
-                    } 
-                    data = content.replace('HTML_EXTRA', html_extra);
-                }
-                res.writeHead(resCode, {'Content-Type': contentType});
-                res.end(data);
-            }
-        });
+            });
+        }
     }
 });
 
