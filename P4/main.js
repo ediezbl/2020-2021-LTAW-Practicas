770f14bcbca1567cc09f29afff66616438f30053
ediez <eduardodiezblanco@gmail.com>
2021-05-31 11:56:35 +0200

    Creando la ventana principal

--- P4/main.js
+++ P4/main.js
@@ -4,8 +4,17 @@ const electron = require('electron');
 
 console.log("Arrancando Electron ...");
 
+// Variable para acceder a la ventana principal
+// Se pone aqui para que sea global al modulo principal
+let win = null;
 // Punto de entrada. En cuanto electron este listo,
 // Ejecuta esta funcion 
 electron.app.on('ready', () => {
     console.log("Evento Ready!");
+    // Crear la ventana principal de nuestra aplicacion 
+    win = new electron.BrowserWindow ({
+        width: 600, // Esto es la anchura 
+        height: 400 // Esto es la altura 
+    });
 })
+
