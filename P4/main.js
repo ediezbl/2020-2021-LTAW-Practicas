1231d73b3af54ad1ecadf5a9eeeb4a451888b5ef
ediez <eduardodiezblanco@gmail.com>
2021-05-31 14:16:21 +0200

    Acceso al sistema desde las aplicaciones de electron

--- P4/main.js
+++ P4/main.js
@@ -1,25 +1,33 @@
 
-// Cargar el modulo de electron
+//-- Cargar el módulo de electron
 const electron = require('electron');
 
-console.log("Arrancando Electron ...");
+console.log("Arrancando electron...");
 
-// Variable para acceder a la ventana principal
-// Se pone aqui para que sea global al modulo principal
+//-- Variable para acceder a la ventana principal
+//-- Se pone aquí para que sea global al módulo principal
 let win = null;
-// Punto de entrada. En cuanto electron este listo,
-// Ejecuta esta funcion 
+
+//-- Punto de entrada. En cuanto electron está listo,
+//-- ejecuta esta función
 electron.app.on('ready', () => {
     console.log("Evento Ready!");
-    // Crear la ventana principal de nuestra aplicacion 
-    win = new electron.BrowserWindow ({
-        width: 600, // Esto es la anchura 
-        height: 400 // Esto es la altura 
+
+    //-- Crear la ventana principal de nuestra aplicación
+    win = new electron.BrowserWindow({
+        width: 600,   //-- Anchura 
+        height: 600,  //-- Altura
+
+        //-- Permitir que la ventana tenga ACCESO AL SISTEMA
+        webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false
+        }
     });
 
-    // Cargar contenido web en la ventana 
-    // La ventana es en realidad... ¡un navegador!
-    win.loadFile("index.html");
-})
+  //-- Cargar interfaz gráfica en HTML
+  win.loadFile("index.html");
+
+});
  
 
