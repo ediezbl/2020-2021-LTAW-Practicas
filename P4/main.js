b4c2699ea0ae54c031a6fa77e7c3c3cecdc1063c
ediez <eduardodiezblanco@gmail.com>
2021-05-31 11:49:07 +0200

    Añadiendo el evento ready

--- P4/main.js
+++ P4/main.js
@@ -1,4 +1,11 @@
 
+// Cargar el modulo de electron
 const electron = require('electron');
 
 console.log("Arrancando Electron ...");
+
+// Punto de entrada. En cuanto electron este listo,
+// Ejecuta esta funcion 
+electron.app.on('ready', () => {
+    console.log("Evento Ready!");
+})
